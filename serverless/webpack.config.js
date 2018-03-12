c1ba8f60caa53bd48f7b38d71bd22476d558ba83
Cody Swann <cody@gunnertech.com>
2018-03-12 17:08:03 -0400

    minr

--- serverless/webpack.config.js
+++ serverless/webpack.config.js
@@ -1,37 +1,26 @@
-const MinifyPlugin = require("babel-minify-webpack-plugin");
-
-const minifyOpts = {};
-const pluginOpts = {};
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
 
 module.exports = {
-    // entry: set by the plugin
-    // output: set by the plugin
-    plugins: [
-        new MinifyPlugin(minifyOpts, pluginOpts)
-    ],
-    target: 'node',
-    externals: [
-        /aws-sdk/, // Available on AWS Lambda
-    ],
-    // module: {
-    //     rules: [{
-    //         test: /\.js$/,
-    //         exclude: /node_modules/,
-    //         loader: 'babel-loader',
-    //         query: {
-    //             presets: [
-    //                 [
-    //                     'env',
-    //                     {
-    //                         target: { node: '6.10' }, // Node version on AWS Lambda
-    //                         useBuiltIns: true,
-    //                         modules: false,
-    //                         loose: true,
-    //                     },
-    //                 ],
-    //                 'stage-0',
-    //             ],
-    //         },
-    //     }, ],
-    // },
+  entry: slsw.lib.entries,
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+      }
+    ]
+  }
 };
